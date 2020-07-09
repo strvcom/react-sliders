55cb73f92e47d953d7bb5213b19ec205c0e4f120
Lukas Hudec <hudec.luke@gmail.com>
2020-07-09 15:53:12 +0200

    chore: add support for path aliases

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   setupFilesAfterEnv: ['./config/jest.setup.ts'],
+  moduleNameMapper: {
+    '^components(.*)$': '<rootDir>/src/component$1',
+    '^hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^utils(.*)$': '<rootDir>/src/utils$1',
+  },
 }
