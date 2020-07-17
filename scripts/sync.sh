e988f4d7703bacb0aea1db5a5c2bc19b8a412d8e
Lukas Hudec <hudec.luke@gmail.com>
2020-07-17 09:12:59 +0200

    chore: add deployment workflow

---
+++ scripts/sync.sh
@@ -0,0 +1,26 @@
+# Thank you @robertrossmann!
+# @see https://github.com/strvcom/heimdall/blob/3107d4ca279bb17dc0ac6d83a9b9549670075233/utils/ci/sync.sh
+
+#!/bin/sh
+
+set -o errexit
+
+head=$(git rev-parse --abbrev-ref HEAD)
+
+printf "\n=====>\tFetching new commits...\n"
+
+# Ensure we are merging the release into current branch tips to avoid pushes being rejected
+git fetch origin
+
+printf "\n=====>\tMerging to master: %s...\n" "${head}"
+
+git checkout master
+git merge "${head}"
+
+# Print the repo status after merging, for troubleshooting purposes
+git status
+git log --oneline HEAD~10..HEAD
+
+printf "\n=====>\tPushing...\n"
+
+git push origin master
